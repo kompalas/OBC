634e4c10a4390e31ee27591c23fadc46e87c1508
KonstantinosBalaskas <kompalas@physics.auth.gr>
2023-07-06 14:20:00 -0500

    fix for quant torch 1.3

--- quant.py
+++ quant.py
@@ -47,11 +47,12 @@ class Quantizer(nn.Module):
             x = x.flatten().unsqueeze(0)
 
         tmp = torch.zeros(x.shape[0], device=dev)
-        xmin = torch.minimum(x.min(1)[0], tmp)
-        xmax = torch.maximum(x.max(1)[0], tmp)
+        # NOTE: Change torch from 'minimum'/'maximum' to 'min/'max'
+        xmin = torch.min(x.min(1)[0], tmp)
+        xmax = torch.max(x.max(1)[0], tmp)
 
         if self.sym:
-            xmax = torch.maximum(torch.abs(xmin), xmax)
+            xmax = torch.max(torch.abs(xmin), xmax)
             tmp = xmin < 0
             if torch.any(tmp):
                 xmin[tmp] = -xmax[tmp]
